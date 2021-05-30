f65bc9fd3a4a21e730744776b574c949ceee1e36
joiellantero <jtllantero@up.edu.ph>
2021-05-30 20:37:03 +0800

    fix bug
    now able to handle superscript and subscript

--- main.py
+++ main.py
@@ -7,13 +7,27 @@ class Node:
         self.next = None
 
 
+# displaying the linked list for testing
 def display(head):
     current = head
-    # print('encoded message (linked list form):')
     while current is not None:
         print(current.data, end=" -> ")
         current = current.next
     print('\n')
+
+
+# similar to isdigit() or isnumeric() methods 
+# however, this function extends those methods to be able to check for superscripts and subscripts.
+def is_numeric(data):
+    count = 0
+    for i in data:
+        if ord(i) >= ord('0') and ord(i) <= ord('9'):
+            continue 
+        else:
+            count += 1
+    if count > 0:
+        return False 
+    return True
 
 
 def generate_linked_list(text):
@@ -28,7 +42,8 @@ def generate_linked_list(text):
 
         for j in range(len(text) - 1):
             temp = text[j + 1]
-            if current.data.isdigit() and ord(temp) >= ord('0') and ord(temp) <= ord('9'):
+            # since integers can be two digits or more, I have added this if else statement to address that.
+            if is_numeric(current.data) and ord(temp) >= ord('0') and ord(temp) <= ord('9'):
                 current.data += text[j + 1]
             else:
                 current.next = add(text[j + 1])
@@ -42,11 +57,14 @@ def generate_linked_list(text):
     return head
 
 
+# Since the current methods in python like round() doesnt round up number like 1.5 or 2.5,
+# I added this function which also rounds them up to the nearest whole number.
 def round_half_up(n, decimals=0):
     multiplier = 10 ** decimals
     return int(math.floor(n * multiplier + 0.5) / multiplier)
 
 
+# this function takes in the comprerssed messages by the user and uncompresses it.
 def decode(com_msg, head):
     parent = head
     uncom_msg = ""
@@ -54,27 +72,37 @@ def decode(com_msg, head):
 
     while (parent != None):
         temp = parent.data
-        if temp.isdigit():
+        # if the current node is a digit, we take the next node, repeat it 'temp' times, and append the result to the 'uncom_msg' string variable
+        if is_numeric(temp):
             char = parent.next
             for i in range(0, int(temp)-1):
                 uncom_msg += char.data
+
+        # if 'temp' is not a number, then we just append it to the uncom_msg since there's no need to repeat it.
         else:
             uncom_msg += temp
         parent = parent.next
 
-    # print(len(uncom_msg)/len(com_msg), com_msg, '-> ', uncom_msg)
+    # compute for the compression ratio to check the number of time the data has been reduced.
     c = round_half_up(len(uncom_msg)/len(com_msg))
+
+    # we return both the uncompressed message and the compression ratio to be displayed on the terminal.
     res.append(uncom_msg)
     res.append(c)
-    # print('decoded message (string form):')
     return res
 
 
 if __name__ == '__main__':
+    # obtain the amount of inputs we'll take from the user
     n = int(input())
     enc_list = []
+
+    # obtain the compressed messages from the user
     for i in range(n):
         enc_list.append(input())
+
+    # put the compressed messages into a linked list then pass it to the decode function to be uncompressed
+    # linked list was used to separate the integers from the characters for easier access.
     for item in enc_list:
         res = decode(item, generate_linked_list(item))
         print(res[1], res[0])
